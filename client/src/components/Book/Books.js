e3f9abcb65a53ceff0cc58992d6ee6e2c5a61bc1
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-07-28 19:05:49 +0600

    show books and singlebook, about

--- client/src/components/Book/Books.js
+++ client/src/components/Book/Books.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import SingleBook from "./SingleBook";
+import "./Books.css";
 
 const URL = "http://localhost:5000/books";
 
@@ -11,10 +13,21 @@ const Books = () => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    fetchBooks().then((data) => setBooks(data));
+    fetchBooks().then((data) => setBooks(data.books));
   }, []);
   console.log(books);
-  return <div>Books</div>;
+  return (
+    <div>
+      <ul>
+        {books &&
+          books.map((book, i) => (
+            <div className="book" key={i}>
+              <SingleBook book={book} />
+            </div>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Books;
