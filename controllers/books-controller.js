05c50a88e80ac86fa52d7c3c75a64586882c12a4
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-07-28 16:34:13 +0600

    created books-controller

---
+++ controllers/books-controller.js
@@ -0,0 +1,18 @@
+const Book = require("../models/Book");
+
+const getAllBooks = async (req, res, next) => {
+  let books;
+  try {
+    books = await Book.find();
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (!books) {
+    return res.status(404).json({ message: "No books found" });
+  }
+
+  return res.status(200).json({ books });
+};
+
+exports.getAllBooks = getAllBooks;
--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,21 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Book = require("../models/Book");
+const bookController = require("../controllers/book-controller");
 
-router.get("/", async (req, res, next) => {
-  // This route will provide all of the books
-  let books;
-  try {
-    books = await Book.find();
-  } catch (err) {
-    console.log(err);
-  }
-
-  if (!books) {
-    return res.status(404).json({ message: "No books found" });
-  }
-
-  return res.status(200).json({ books });
-});
+router.get("/", bookController.getAllBooks);
 
 module.exports = router;
